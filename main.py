c750af49e72ab0737e7e163401d7cfe11428eb8d
lsyzg <lucas.wiz.average@gmail.com>
2024-04-21 22:15:33 -0400

    updated ema_calc, adx_calc, macd_calc, rsi_calc, and updated package list
    wrote macd_calc, rsi_calc, and rewrote ema_calc. had adx_calc return the actual adx value instead of a dataframe. uninstalled matplotlib from venv

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import pandas as pd
 from datetime import date
 from datetime import timedelta
 from datetime import datetime
-import matplotlib.pyplot as plt
 
 pd.options.mode.chained_assignment = None
 
@@ -23,6 +22,7 @@ def obv_calc(dataframe, period: int):
     obvs = []
     obvys = [1,2,3,4,5,6]
     dates = []
+
     for i in range(period):
         dates.append(str(tdate - timedelta(days = i + 1)))
 
@@ -55,6 +55,7 @@ def adl_calc(dataframe, period: int):
     adlys = [1,2,3,4,5,6]
     dates = []
     day = 0
+
     for i in range(period):
         dates.append(str(tdate - timedelta(days = i + 1)))
 
@@ -105,23 +106,75 @@ def adx_calc(dataframe, period: int):
     df['DX'] = (np.abs(df['+DMI'] - df['-DMI']) / (df['+DMI'] + df['-DMI'])) * 100
     df['ADX'] = df['DX'].ewm(alpha=alpha, adjust=False).mean()
     del df['DX'], df['ATR'], df['TR'], df['-DX'], df['+DX'], df['+DMI'], df['-DMI']
-    adx = df[["ADX"]]
-    adx = adx.dropna()
+    adx_df = df[["ADX"]]
+    adx_df = adx_df.dropna()
+    adx = adx_df.iat[-1,0]
     return adx
 
-def ema_calc(dataframe, period: int):
+def ema_calc(dataframe, period: int, period_num: int, column_name: str):
+    vccolumns = dataframe[[column_name]]
+    df = vccolumns.iloc[-abs(period * period_num):]
+
+    ema_list = []
+
+    for i in range(period_num):
+        temp_df = df.iloc[i * period:(i + 1) * period]
+        sma = temp_df[column_name].mean()
+        multiplier = 2 / (period + 1)
+        ema = sma
+        for value in temp_df[column_name][1:]:
+            ema = (value - ema) * multiplier + ema
+        ema_list.append(ema)
+    if period_num == 1:
+        return ema_list[0]
+    else:
+        return ema_list
+
+def macd_calc(dataframe, num_macds: int):
+    # num_macds should be a multiple of 9 and a minimum of 18 to ensure that a signal line can be calculated
+    vccolumns = dataframe[["Close"]]
+    df = vccolumns.iloc[-abs(num_macds * 26):]
+
+    macd_list = []
+
+    for i in range(num_macds):
+        temp_df = df.iloc[i * 26:(i + 1) * 26]
+        macd = ema_calc(temp_df, period=12, period_num=1, column_name="Close") - ema_calc(temp_df, period=26, period_num=1, column_name="Close")
+        macd_list.append(macd)
+
+    macd_df = pd.DataFrame(macd_list, columns=['MACD_values'])
+    period_num = int(len(macd_df) / 9)
+    signal_line_vals = ema_calc(macd_df, period=9, period_num=period_num, column_name="MACD_values")
+
+    if macd_list[-1] > signal_line_vals[-1] and macd_list[-2] < signal_line_vals[-2]:
+        return "bearish"
+    elif macd_list[-1] < signal_line_vals[-1] and macd_list[-2] > signal_line_vals[-2]:
+        return "bullish"
+    else:
+        return "no crosses"
+    
+def rsi_calc(dataframe, period=int):
     vccolumns = dataframe[["Close"]]
     df = vccolumns.iloc[-abs(period):]
 
-    sma = df["Close"].sum() / period
-    multiplier = (2 / (period + 1))
-    fclose = df.iat[0, 0]
-    ema = (fclose - sma) * multiplier + sma
-    for close in df["Close"][1:]:
-        ema = (close - ema) * multiplier + ema
-    return ema
+    gains = []
+    losses = []
 
-def macd_calc(dataframe):
-    macd = ema_calc(dataframe, period=12) - ema_calc(dataframe, period=26)
-    return macd
-print(macd_calc(df))
+    for i in range(1, period):
+        price_diff = df.iat[i, 0] - df.iat[i - 1, 0]
+        if price_diff > 0:
+            gains.append(price_diff)
+            losses.append(0)
+        else:
+            gains.append(0)
+            losses.append(abs(price_diff))
+
+    avg_gain = np.mean(gains)
+    avg_loss = np.mean(losses)
+
+    if avg_loss == 0:
+        return 100
+
+    rs = avg_gain / avg_loss
+    rsi = 100 - (100 / (1 + rs))
+    return rsi
